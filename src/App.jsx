30aa6cd5d7b420883c96efaca4ddc71de6a5f389
annuu1 <mannusng876@gmail.com>
2025-01-17 17:33:11 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import MainContent from './components/MainContent';
 
 function App() {
   const [selectedNote, setSelectedNote] = useState(null);
+  const [notes, setNotes] = useState([{
+    id: 1,
+    title: 'Note 1',
+    content: ['This is the content of note 1', 'this is the second note', 'this is the third note']
+  }]);
 
   return (
     <>
       <div style={{ display: "flex", width: '100%' }}>
-        <Sidebar setSelectedNote={setSelectedNote} />
-        <MainContent selectedNote={selectedNote} />
+        <Sidebar setSelectedNote={setSelectedNote}  />
+        <MainContent selectedNote={selectedNote} notes={notes} setNotes={setNotes}/>
       </div>
     </>
   );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import style from '../styles/MainContent.module.css';
+import React from "react";
+import style from "../styles/MainContent.module.css";
+
+const MainContent = ({ selectedNote, notes, setNotes }) => {
+
+  return (
+    <main style={{ width: "80%",  background: "#DAE5F5" }}>
+      {selectedNote ? (
+        <>
+        <div className={style.notesContainer}>
+            {notes.map((note, index) => {
+
+              if (note.id.toString() === "1") {
+                return (
+                    note.content.map((content)=>{
+                        return (
+                            <div className={style.noteContainer} key={index}>
+                            <p>{content}</p>
+                          </div>
+                        )
+                    })
+                );
+              }
+            })}
 
 
-const MainContent = ({ selectedNote }) => {
-    return (
-        <main style={{ width: '80%', flex: 1, background: '#DAE5F5' }}>
-            <div className={style.bankPageContent} style={{ width: '50%', textAlign: 'center' }}>
-                <img src="https://s3-alpha-sig.figma.com/img/f2b5/d356/00b6d4748cd536df01bd2b4fecc1d821?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NfQR3vqqO9Hwzxtx-MkI44tAhDebMQVPitweha1WjhgTLw764IXurUsVqjR2Ql3b6VSNghWTTgaPWgeG-M1-5v88wekaD32W9DCgEUVtpfTzeppQaznJEvCcD~4wtLIokhn78EI5~uCZ1~FwMBf-aDLN0iqqJIEjxr67HJKZWwsaq~LrJTWqOA0b9wW2doyn1GSS4r1PVQjRCtIyfgIxJ-mttE3gQFC07G6YxuDshDopRLlhytwZ-NxKrlz2whNQ~lAywYBO3w6y6Yk8GVKQCVASndqxARBbOFnabJck81tx~WNzuuoCYVVdNecZtHx49Nn9eQD82JPvyVgemYvvWQ__" alt="img not found" />
-                <h2>Pocket Notes</h2>
-                {selectedNote ? (
-                    <p>You selected: {selectedNote}</p>
-                ) : (
-                    <p>Send and receive messages without keeping your phone online.<br />
-                    Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
-                )}
-            </div>
-        </main>
-    );
+          </div>
+          <textarea name="addNote" id="addNote"></textarea>
+        </>
+      ) : (
+        <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+                <div
+          className={[style.bankPageContent]}
+          style={{ width: "50%", textAlign: "center"}}>
+
+          <h2>Pocket Notes</h2>
+          <>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/f2b5/d356/00b6d4748cd536df01bd2b4fecc1d821?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NfQR3vqqO9Hwzxtx-MkI44tAhDebMQVPitweha1WjhgTLw764IXurUsVqjR2Ql3b6VSNghWTTgaPWgeG-M1-5v88wekaD32W9DCgEUVtpfTzeppQaznJEvCcD~4wtLIokhn78EI5~uCZ1~FwMBf-aDLN0iqqJIEjxr67HJKZWwsaq~LrJTWqOA0b9wW2doyn1GSS4r1PVQjRCtIyfgIxJ-mttE3gQFC07G6YxuDshDopRLlhytwZ-NxKrlz2whNQ~lAywYBO3w6y6Yk8GVKQCVASndqxARBbOFnabJck81tx~WNzuuoCYVVdNecZtHx49Nn9eQD82JPvyVgemYvvWQ__"
+              alt="img not found"
+            />
+            <p>
+              Send and receive messages without keeping your phone online.
+              <br />
+              Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+            </p>
+
+          </>
+        </div>
+        </div>
+        
+      )}
+    </main>
+  );
 };
 
 export default MainContent;
